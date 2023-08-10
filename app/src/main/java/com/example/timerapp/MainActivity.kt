f3ae96a7da76c0017474f05fa2fb743680c57e64
mametchi <tinyaqui@gmail.com>
2023-08-10 19:13:52 +0300

    AlarmService version

--- app/src/main/java/com/example/timerapp/MainActivity.kt
+++ app/src/main/java/com/example/timerapp/MainActivity.kt
@@ -1,169 +1,172 @@
-package com.example.timerapp
+    package com.example.timerapp
 
-import AlarmService
-import MyWorker
-import TimerViewModel
-import android.app.ActivityManager
-import android.app.AlarmManager
-import android.app.PendingIntent
-import android.content.Context
-import android.content.Intent
-import android.content.res.Configuration
-import android.net.Uri
-import android.os.Bundle
-import android.os.PowerManager
-import android.provider.Settings
-import android.view.View
-import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import androidx.work.Constraints
-import androidx.work.NetworkType
-import androidx.work.OneTimeWorkRequest
-import com.example.timerapp.databinding.ActivityMainBinding
-import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Locale
-import java.util.concurrent.TimeUnit
+    import TimerViewModel
+    import androidx.lifecycle.ViewModelProvider
+    import android.app.AlarmManager
+    import android.app.PendingIntent
+    import android.content.Intent
+    import android.content.pm.PackageManager
+    import android.media.MediaPlayer
+    import android.os.Bundle
+    import android.view.View
+    import androidx.appcompat.app.AppCompatActivity
+    import androidx.core.app.ActivityCompat
+    import androidx.core.content.ContextCompat
+    import android.Manifest
+    import androidx.lifecycle.Observer
+    import com.example.timerapp.AlarmReceiver
+    import com.example.timerapp.databinding.ActivityMainBinding
+    import java.text.SimpleDateFormat
+    import java.util.Date
+    import java.util.Locale
 
-import androidx.work.*
+    class MainActivity : AppCompatActivity() {
 
-class MainActivity : AppCompatActivity() {
+        private val isTimerRunning: Boolean
+        private var alarmManager: AlarmManager? = null
+        private var pendingIntent: PendingIntent? = null
+        private var alarmPlayer: MediaPlayer? = null
 
+        private lateinit var binding: ActivityMainBinding
+        private lateinit var timerViewModel: TimerViewModel
 
-    object MySingleton {
-        var alarmOn: Boolean = false
-    }
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
 
-    private lateinit var binding: ActivityMainBinding
-    private lateinit var timerViewModel: TimerViewModel
+            binding = ActivityMainBinding.inflate(layoutInflater)
+            setContentView(binding.root)
+            timerViewModel = ViewModelProvider(this).get(TimerViewModel::class.java)
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+            // Observe the timer LiveData and update the TextView
+            timerViewModel.currentTime.observe(this, Observer { timeInMillis ->
+                updateTimerText(timeInMillis)
+            })
 
 
+            if (savedInstanceState != null) {
+                timerViewModel.restoreInstanceState(savedInstanceState)
+            } else {
+                timerViewModel.startTimer(0)
+            }
 
+            requestAlarmPermission()
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        timerViewModel = ViewModelProvider(this).get(TimerViewModel::class.java)
-
-        // Observe the timer LiveData and update the TextView
-        timerViewModel.currentTime.observe(this, Observer { timeInMillis ->
-            updateTimerText(timeInMillis)
-        })
-
-
-        if (savedInstanceState != null) {
-            timerViewModel.restoreInstanceState(savedInstanceState)
-        } else {
-            timerViewModel.startTimer(0)
+            // Initialize the AlarmManager
+            alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
         }
 
-    }
-        // Set a click listener on a button to start the timer with a specified time
 
-    private var myWorkerRequest: WorkRequest? = null
+        private fun updateTimerText(timeInMillis: Long) {
+            val format = SimpleDateFormat("mm:ss", Locale.getDefault())
+            val formattedTime = format.format(Date(timeInMillis))
+            binding.textView.text = formattedTime
+        }
 
-    private fun isServiceRunning(serviceClass: Class<*>): Boolean {
-        val manager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
-        for (service in manager.getRunningServices(Int.MAX_VALUE)) {
-            if (serviceClass.name == service.service.className) {
-                return true
+        fun playAlarmIn5Minutes(view: View?) {
+            var delay = 5
+            scheduleAlarm(delay)
+            if (isTimerRunning) {
+                timerViewModel.resetTimer(seconds * 1000L)
+            } else {
+                isTimerRunning = true
+                timerViewModel.startTimer(delay * 1000L)
             }
         }
-        return false
-    }
 
-    private fun stopAlarmService() {
-        val stopIntent = Intent(this, AlarmService::class.java)
-        stopService(stopIntent)
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        timerViewModel.saveInstanceState(outState)
-    }
-    override fun onConfigurationChanged(newConfig: Configuration) {
-        super.onConfigurationChanged(newConfig)
-        setContentView(R.layout.activity_main)
-    }
-
-    private fun updateTimerText(timeInMillis: Long) {
-        val format = SimpleDateFormat("mm:ss", Locale.getDefault())
-        val formattedTime = format.format(Date(timeInMillis))
-        binding.textView.text = formattedTime
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        timerViewModel.stopTimer()
-    }
-
-
-    private fun enqueueMyWorkerWithDelay(delay: Long) {
-        // Cancel the previous CoroutineWorker if it exists
-        if (myWorkerRequest != null) {
-            WorkManager.getInstance(this).cancelWorkById(myWorkerRequest!!.id)
-            myWorkerRequest = null
+        fun playAlarmIn10Minutes(view: View?) {
+            var delay = 10
+            scheduleAlarm(delay)
+            if (isTimerRunning) {
+                timerViewModel.resetTimer(seconds * 1000L)
+            } else {
+                isTimerRunning = true
+                timerViewModel.startTimer(delay * 1000L)
+            }
         }
 
-        // Cancel the AlarmService if it is running
-        if (isServiceRunning(AlarmService::class.java)) {
-            stopAlarmService()
+        fun playAlarmIn15Minutes(view: View?) {
+            var delay = 15
+            scheduleAlarm(delay)
+            if (isTimerRunning) {
+                timerViewModel.resetTimer(seconds * 1000L)
+            } else {
+                isTimerRunning = true
+                timerViewModel.startTimer(delay * 1000L)
+            }
         }
 
-        // Create constraints (if needed)
-        val constraints = Constraints.Builder()
-            .setRequiredNetworkType(NetworkType.NOT_REQUIRED)
-            .build()
+        fun playAlarmIn20Minutes(view: View?) {
+            var delay = 20
+            scheduleAlarm(delay)
+            if (isTimerRunning) {
+                timerViewModel.resetTimer(seconds * 1000L)
+            } else {
+                isTimerRunning = true
+                timerViewModel.startTimer(delay * 1000L)
+            }
+        }
 
-        // Pass the delay as an input data for MyWorker
-        val inputData = Data.Builder()
-            .putLong("delay", delay)
-            .build()
+        private fun scheduleAlarm(seconds: Int) {
+            // Cancel any previous alarm
+            cancelAlarm()
 
-        // Create the one-time work request
-        val workRequest = OneTimeWorkRequest.Builder(MyWorker::class.java)
-            .setInputData(inputData) // Pass the input data with delay
-            .setConstraints(constraints)
-            .setBackoffCriteria(BackoffPolicy.EXPONENTIAL, delay * 2, TimeUnit.MILLISECONDS)
-            .build()
+            // Calculate the alarm time in milliseconds
+            val timeInMillis = System.currentTimeMillis() + seconds * 1000
 
-        // Enqueue the work and save the request for later use
-        WorkManager.getInstance(this).enqueue(workRequest)
-        myWorkerRequest = workRequest
+            // Create an intent to launch the AlarmReceiver
+            val intent = Intent(this, AlarmReceiver::class.java)
+            val pendingIntent = PendingIntent.getBroadcast(this, 0, intent, 0)
+
+            // Schedule the alarm using AlarmManager
+            alarmManager?.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, timeInMillis, pendingIntent)
+        }
+        private fun cancelAlarm() {
+            // Cancel any previous alarm
+            if (alarmManager != null && pendingIntent != null) {
+                alarmManager!!.cancel(pendingIntent!!)
+                pendingIntent!!.cancel()
+            }
+        }
+
+        override fun onDestroy() {
+            super.onDestroy()
+            // Cancel any scheduled alarms and release MediaPlayer resources
+            cancelAlarm()
+            stopAlarm()
+
+        }
+
+        private fun stopAlarm() {
+            // Stop and release the MediaPlayer resources
+            if (alarmPlayer != null) {
+                alarmPlayer!!.stop()
+                alarmPlayer!!.release()
+                alarmPlayer = null
+            }
+        }
+
+        private val PERMISSION_REQUEST_CODE = 1
+
+        override fun onSaveInstanceState(outState: Bundle) {
+            super.onSaveInstanceState(outState)
+            timerViewModel.saveInstanceState(outState)
+        }
+
+        override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+            super.onRestoreInstanceState(savedInstanceState)
+            timerViewModel.restoreInstanceState(savedInstanceState)
+
+        }
+
+        private fun requestAlarmPermission() {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.SET_ALARM)
+                != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this, arrayOf<String>(Manifest.permission.SET_ALARM),
+                    PERMISSION_REQUEST_CODE
+                )
+            }
+        }
     }
-
-    fun startAlarm_5(view: View?) {
-        val delay: Long = (5 * 60 * 1000).toLong() // 5 minutes in milliseconds
-        enqueueMyWorkerWithDelay(delay)
-        timerViewModel.resetTimer(delay)
-        Toast.makeText(this, "Alarm set for 5 minutes later.", Toast.LENGTH_SHORT).show()
-    }
-
-    fun startAlarm_10(view: View?) {
-        val delay: Long = 10 * 60 * 1000 // 10 minutes in milliseconds
-        enqueueMyWorkerWithDelay(delay)
-        timerViewModel.resetTimer(delay)
-        Toast.makeText(this, "Alarm set for 10 minutes later.", Toast.LENGTH_SHORT).show()
-    }
-
-    fun startAlarm_15(view: View?) {
-        val delay: Long = 15 * 60 * 1000 // 10 minutes in milliseconds
-        enqueueMyWorkerWithDelay(delay)
-        timerViewModel.resetTimer(delay)
-        Toast.makeText(this, "Alarm set for 15 minutes later.", Toast.LENGTH_SHORT).show()
-    }
-    fun startAlarm_20(view: View?) {
-        val delay: Long = 20 * 60 * 1000 // 10 minutes in milliseconds
-        enqueueMyWorkerWithDelay(delay)
-        timerViewModel.resetTimer(delay)
-        Toast.makeText(this, "Alarm set for 20 minutes later.", Toast.LENGTH_SHORT).show()
-    }
-
-}
